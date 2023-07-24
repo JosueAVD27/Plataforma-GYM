a82c76a5e04f69d45297a23ffb7b89fa19e7451b
BryanDav1d <90330674+BryanDav1d@users.noreply.github.com>
2023-07-24 01:17:45 -0500

    Update DataController.java

--- appfit/src/main/java/com/persona/appfit/controllers/DataController.java
+++ appfit/src/main/java/com/persona/appfit/controllers/DataController.java
@@ -1,36 +1,24 @@
 package com.persona.appfit.controllers;
 
+import com.persona.appfit.services.SuplementoService;
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
-@RequestMapping
+@RequestMapping("/suplementacion")
 public class DataController {
-    private final MongoTemplate mongoTemplate;
+    private final SuplementoService suplementoService;
+
     @Autowired
-    public DataController(MongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+    public DataController(SuplementoService suplementoService) {
+        this.suplementoService = suplementoService;
     }
-    @GetMapping("/suplementacion")
+
+    @GetMapping
     public List<Document> getSuplementos() {
-        String url = "https://kejd9.mocklab.io/suplementos";
-        RestTemplate restTemplate = new RestTemplate();
-        // Consumir el API y obtener los datos en un arreglo de objetos
-        List<Map<String, Object>> suplementacion = restTemplate.getForObject(url, List.class);
-        // Eliminar los datos existentes en la colección de suplementos antes de guardar los nuevos
-        mongoTemplate.dropCollection("suplementos");
-        // Guardar cada suplemento en MongoDB
-        for (Map<String, Object> suplementoData : suplementacion) {
-            Document suplementoDocument = new Document(suplementoData);
-            mongoTemplate.insert(suplementoDocument, "suplementos");
-        }
-        // Opcionalmente, puedes devolver los documentos guardados si lo necesitas
-        return mongoTemplate.findAll(Document.class, "suplementos");
+        return suplementoService.obtenerSuplementos();
     }
 }
