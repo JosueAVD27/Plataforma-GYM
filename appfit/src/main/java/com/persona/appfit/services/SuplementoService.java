814eabdb4cbb60a29e343623eed2947fa6ef275b
BryanDav1d <90330674+BryanDav1d@users.noreply.github.com>
2023-07-24 01:17:11 -0500

    Update SuplementoService.java

--- appfit/src/main/java/com/persona/appfit/services/SuplementoService.java
+++ appfit/src/main/java/com/persona/appfit/services/SuplementoService.java
@@ -21,15 +21,16 @@ public class SuplementoService {
     public List<Document> obtenerSuplementos() {
         String url = "https://kejd9.mocklab.io/suplementos";
         RestTemplate restTemplate = new RestTemplate();
+        // Consumir el API y obtener los datos en un arreglo de objetos
         List<Map<String, Object>> suplementacion = restTemplate.getForObject(url, List.class);
-
+        // Eliminar los datos existentes en la colección de suplementos antes de guardar los nuevos
         mongoTemplate.dropCollection("suplementos");
-
+        // Guardar cada suplemento en MongoDB
         for (Map<String, Object> suplementoData : suplementacion) {
             Document suplementoDocument = new Document(suplementoData);
             mongoTemplate.insert(suplementoDocument, "suplementos");
         }
-
+        // Opcionalmente, puedes devolver los documentos guardados si lo necesitas
         return mongoTemplate.findAll(Document.class, "suplementos");
     }
 }
